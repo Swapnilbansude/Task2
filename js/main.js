3b5c69f18ea21818fe1e05bbec60e758efb11475
Swapnilbansude <59506096+Swapnilbansude@users.noreply.github.com>
2021-04-04 21:34:06 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+function fileValidation() {
+    var fileInput1 = document.getElementById('exampleFormControlFile1');
+    var fileInput2 = document.getElementById('exampleFormControlFile2');
 
+    var filePath1 = fileInput1.value;
+    var filePath2 = fileInput1.value;
+
+    // Allowing file type
+    var allowedExtensions =
+        /(\.csv|\.CSV)$/i;
+
+    if (!allowedExtensions.exec(filePath1) || !allowedExtensions.exec(filePath2)) {
+        alert('Invalid file type(Upload CSV Files only)');
+        fileInput1.value = '';
+        fileInput2.value = '';
+        return false;
+    }
+
+}
+
+function validate() {
+    var fileInput1 = document.getElementById('exampleFormControlFile1');
+    var fileInput2 = document.getElementById('exampleFormControlFile2');
+
+    var filePath1 = fileInput1.value;
+    var filePath2 = fileInput1.value;
+
+    if (filePath1 == 0) {
+        alert("Please select files");
+    }
+
+}
